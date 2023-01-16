10918226f64dc63373c679b1d66ae4e38d8bb367
Amonsage <fabio.marani98@gmail.com>
2023-01-16 13:01:58 +0100

    baule e vaso funzionanti

---
+++ Progetto/Assets/ChestController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace er06 {
+
+    public class ChestController : MonoBehaviour
+    {
+        public UnityEvent OnChestOpen;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Key"))
+            {
+                Debug.Log("SUDSSAUAH");
+                OnChestOpen.Invoke();
+
+            }
+        }
+    }
+
+
+
+}
